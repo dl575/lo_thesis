03bec32a9d799172b153c5afec01b4c725036123
Daniel Lo <dl575@cornell.edu>
2015-01-20 14:51:19 -0500

    Clean up Sec 2.5. Make figs for results.

---
+++ monitoring_wcet/data/sequential-wcet.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+sys.path.extend(['../..'])
+import tsg_plot
+import math
+
+# Parse data
+f = open("sequential-wcet.csv", 'r')
+subcat = f.readline().strip().split(',')[1:]
+cat = []
+data = []
+for line in f:
+  if line.strip():
+    ls = line.split(',')
+    cat.append(ls[0])
+    data.append([float(x) for x in ls[1:]])
+f.close()
+
+# Set up plotting options
+opts = tsg_plot.PlotOptions()
+opts.data = data
+opts.labels = [cat, subcat]
+
+attribute_dict = \
+    {
+        'show' : False,
+        'file_name' : 'sequential-wcet.pdf',
+        'paper_mode' : True,
+        'figsize' : (3.5, 1.5),
+        'ylabel' : 'wcet : sim',
+        'legend_ncol' : 4,
+        'legend_handlelength' : 1.0,
+        'legend_columnspacing' : 0.8,
+        'legend_handletextpad' : 0.5,
+        'legend_bbox' : [-0.05, 1.05, 1, 0.1],
+        'rotate_labels' : False,
+        'rotate_labels_angle' : -30,
+        'fontsize' : 8,
+
+        'colors' : ['#eff3ff', '#bdd7e7', '#6baed6', '#2171b5', '#eff3ff', '#bdd7e7', '#6baed6', '#2171b5'],
+        'hatch' : ['']*4 + ['//']*4
+    }
+for name, value in attribute_dict.iteritems():
+  setattr( opts, name, value )
+
+# Plot
+tsg_plot.add_plot( opts )
